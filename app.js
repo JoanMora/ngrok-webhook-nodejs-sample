b08e80748bfc0a1ea7e4975b6e6e616b9e53e767
JoanMora <joan@guruwalk.com>
2024-10-10 11:22:42 +0200

    Use modules

--- app.js
+++ app.js
@@ -1,17 +1,49 @@
-var express = require('express');
-var app = express();
+import express from 'express';
+import chalk from 'chalk';
+
+
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const filterRequest = (req) => {
+  const event = req.body;
+
+  return true;
+}
+
 const port = 3000;
 
+/*
+ // Start Generation Here
+const localtunnel = require('localtunnel');
+
+(async () => {
+  const tunnel = await localtunnel({ port: port });
+  console.log(`Tunnel is open at ${tunnel.url}`);
+
+  tunnel.on('close', () => {
+    console.log('Tunnel closed');
+  });
+})();
+*/
+
+ // Start of Selection
 app.all('/*', function (req, res) {
-   console.log("-------------- New Request --------------");
-   console.log("Headers:"+ JSON.stringify(req.headers, null, 3));
-   console.log("Body:"+ JSON.stringify(req.body, null, 3));
-   res.json({ message: "Thank you for the message" });
+   const event = req.body;
+   if (filterRequest(req)) {
+         // Start of Selection
+      console.log(`-------------- ${event.type} --------------`);
+      // console.log("Headers:"+ JSON.stringify(req.headers, null, 3));
+      const color = event.type === 'page' ? chalk.cyan : event.type === 'track' ? chalk.green : chalk.white;
+      console.log(color(JSON.stringify(req.body, null, 2)));
+      console.log("-----------------------------------------------");
+      res.status(200).json({ message: "Processed" }).send();
+   } else {
+      res.status(200).json({ message: "Not Processed" }).send();
+   }
 })
 
 app.listen(port, function () {
-   console.log(`Example app listening at ${port}`)
+   console.log(`Rudder Debugger listening at ${port}`)
 })
